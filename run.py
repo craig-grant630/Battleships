1c700041102f3be91aec75e3a900c0dbe2ccea31
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2024-10-28 15:47:17 +0000

    Create function that joins the list together with spaces and prints to display users board

--- run.py
+++ run.py
@@ -1,11 +1,20 @@
 BOARD_SIZE = 10
 
 def create_board():
-    # A function to create a list of 
+    #create a list of 
     #list with a borard size of 10
     return [["O"]*10 for x in range(BOARD_SIZE)]
+
+def print_display_boards(user_board):
+    #Print the boards the user will see and update
+    #Iterate through each row and join the list with spaces
+    for i in range(BOARD_SIZE):
+        user_board_row = (" ").join(user_board[i])
+        print(user_board_row)
 
 def play_game():
     computer_board = create_board()
     user_board = create_board()
+    print_display_boards(user_board)
 
+play_game()
