d9c99e77ca5c02243e73ba5d28ccc0f9afa8ebe1
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2024-10-28 15:51:11 +0000

    Add to function print_display_boards to be able to print both user and computer grid

--- run.py
+++ run.py
@@ -5,16 +5,17 @@ def create_board():
     #list with a borard size of 10
     return [["O"]*10 for x in range(BOARD_SIZE)]
 
-def print_display_boards(user_board):
+def print_display_boards(user_board, computer_board):
     #Print the boards the user will see and update
     #Iterate through each row and join the list with spaces
     for i in range(BOARD_SIZE):
         user_board_row = (" ").join(user_board[i])
-        print(user_board_row)
+        computer_board_row = (" ").join(computer_board[i])
+        print(f"{user_board_row}    {computer_board_row}")
 
 def play_game():
     computer_board = create_board()
     user_board = create_board()
-    print_display_boards(user_board)
+    print_display_boards(user_board, computer_board)
 
 play_game()
