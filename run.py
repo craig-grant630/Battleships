b2f8ad0c2499cd540e2da2e8d398c532c247cbc3
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2024-10-28 16:27:27 +0000

    Add comments to be able to undertsand the next steps

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+import random
+
 BOARD_SIZE = 10
+SHIP_SIZES = [5,4,3,3,2]
+DIRECTION = ["N","S","E","W"]
 
 def create_board():
     #create a list of 
@@ -13,9 +17,26 @@ def print_display_boards(user_board, computer_board):
         computer_board_row = (" ").join(computer_board[i])
         print(f"{user_board_row}    {computer_board_row}")
 
+#def computers_ship_placement():
+#loop through different ship sizes
+#for ship_size in SHIP_SIZES:
+#placed = False
+#loop until all ships are placed correclty
+#while not placed:
+#computer_row_place = random.randint(0,BOARD_SIZE- 1)
+#computer_col_place = random.randint(0,BOARD_SIZE -1)
+#computer_direction = random.choice(DIRECTION)
+#Going to need to check see if this can be used - another function required
+#Will next then need to place the ship on the computers board - another function required
+#If checks are good then placed = True 
+
+
+
+
 def play_game():
     computer_board = create_board()
     user_board = create_board()
     print_display_boards(user_board, computer_board)
+
 
 play_game()
