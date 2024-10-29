6f1cda06ebf7d6f5ca29420c6366e529ca8ea88d
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2024-10-29 12:36:27 +0000

    Collect a single ship for testing and place on the computer board using the place function

--- run.py
+++ run.py
@@ -13,30 +13,45 @@ def print_display_boards(user_board, computer_board):
     #Print the boards the user will see and update
     #Iterate through each row and join the list with spaces
     for i in range(BOARD_SIZE):
-        user_board_row = (" ").join(user_board[i])
-        computer_board_row = (" ").join(computer_board[i])
+        user_board_row = ("  ").join(user_board[i])
+        computer_board_row = ("  ").join(computer_board[i])
         print(f"{user_board_row}    {computer_board_row}")
+    print("")
 
-#def computers_ship_placement():
+
 #loop through different ship sizes
-#for ship_size in SHIP_SIZES:
-#placed = False
 #loop until all ships are placed correclty
-#while not placed:
-#computer_row_place = random.randint(0,BOARD_SIZE- 1)
-#computer_col_place = random.randint(0,BOARD_SIZE -1)
-#computer_direction = random.choice(DIRECTION)
 #Going to need to check see if this can be used - another function required
-#Will next then need to place the ship on the computers board - another function required
 #If checks are good then placed = True 
 
+def get_computer_ship(board):
+    print("Computer is placing ship \n")
+    ship_size = 3
+    computer_row_place = random.randint(0,BOARD_SIZE- 1)
+    computer_col_place = random.randint(0,BOARD_SIZE -1)
+    computer_direction = random.choice(DIRECTION)
+    place(computer_row_place, computer_col_place, computer_direction, ship_size, board)
 
-
+def place(row, column, direction, ship_size, board):
+    if direction == "N":
+        for i in range(ship_size):
+            board[row - i][column] = "S"
+    elif direction == "S":
+        for i in range(ship_size):
+            board[row + i][column] = "S"
+    elif direction == "E":
+        for i in range(ship_size):
+            board[row][column + i] = "S"
+    elif direction == "W":
+        for i in range(ship_size):
+            board[row][column - i] = "S"
 
 def play_game():
     computer_board = create_board()
     user_board = create_board()
     print_display_boards(user_board, computer_board)
-
+    get_computer_ship(computer_board)
+    print_display_boards(user_board, computer_board)
+    
 
 play_game()
